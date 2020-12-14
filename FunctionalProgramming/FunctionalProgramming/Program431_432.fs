4bff93365adee24fec90dc34f4d4568c0823ae58
Забелин Михаил Константинович <zabelin.mihail@wildberries.ru>
2020-12-14 21:04:29 +0300

    сделано задание на сложение

---
+++ FunctionalProgramming/FunctionalProgramming/Program431_432.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+//4.3.1 Напишите программу, выводящую строку "Hello, World!!!"
+printfn "Hello world!!!"
+
+//4.3.2. Напишите программу, вычисляющую и выводящую на экран значение выражения 2.1 + 3.2 * (8 / 4 + 1.3)
+let calculate = 2.1 + 3.2 * (float (8 / 4) + 1.3);
+printfn "%f" calculate
