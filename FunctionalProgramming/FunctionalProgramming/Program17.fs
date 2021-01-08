7291a591bed1fbd37cd3b439ed96a43de3e030e7
Забелин Михаил Константинович <zabelin.mihail@wildberries.ru>
2021-01-08 19:39:33 +0300

    сделаны задания группы 17

---
+++ FunctionalProgramming/FunctionalProgramming/Program17.fs
@@ -0,0 +1,20 @@
+﻿module Program17
+
+let rec pow (s, n) = 
+    match n with
+    | 1 -> s
+    | _ -> s + pow(s, n - 1)
+
+
+let rec isIthChar(s: string, n, c) =
+    match s.[n] with
+    | c2 when c2 = c -> true
+    | _ -> false
+
+let rec occFromIthRec(s: string, c, i, acc) = 
+    match i with
+    | len when len = String.length s -> acc
+    | j when s.[j] = c -> occFromIthRec(s, c, i + 1, acc + 1)
+    | _ -> occFromIthRec(s, c, i + 1, acc)
+
+let rec occFromIth(s,n,c) = occFromIthRec(s, n, c, 0)
