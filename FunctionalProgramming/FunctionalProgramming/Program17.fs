89ac037242f5cfc1bfb5949e9bfa04fb9434fd82
Забелин Михаил Константинович <zabelin.mihail@wildberries.ru>
2021-01-16 21:22:42 +0300

    тип аргумента явно задан в функции, вызывающей рекурсивную функцию

--- FunctionalProgramming/FunctionalProgramming/Program17.fs
+++ FunctionalProgramming/FunctionalProgramming/Program17.fs
@@ -6,7 +6,7 @@ let rec pow (s, n) =
     | _ -> s + pow(s, n - 1)
 
 
-let rec isIthChar(s: string, n, c) =
+let isIthChar(s: string, n, c) =
     match s.[n] with
     | c2 when c2 = c -> true
     | _ -> false
@@ -17,4 +17,4 @@ let rec occFromIthRec(s: string, c, i, acc) =
     | j when s.[j] = c -> occFromIthRec(s, c, i + 1, acc + 1)
     | _ -> occFromIthRec(s, c, i + 1, acc)
 
-let rec occFromIth(s,n,c) = occFromIthRec(s, n, c, 0)
+let occFromIth(s: string, n, c) = occFromIthRec(s, n, c, 0)
