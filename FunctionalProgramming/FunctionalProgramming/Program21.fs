df94db5e727f8bdc143e7fa2a5bb0a24639d8ad4
Mikhail Zabelin <mike.rzn@yandex.ru>
2021-02-09 00:19:48 +0300

    сделано задание 21 на карирование

---
+++ FunctionalProgramming/FunctionalProgramming/Program21.fs
@@ -0,0 +1,21 @@
+﻿module Program21
+
+
+(*Задание
+
+Даны две функции
+
+curry: (int * int -> int) -> int -> int ->int
+
+и
+
+uncurry: (int -> int ->int) -> int * int -> int
+curry f -- это функция g, где g x -- это функция h, где h y = f(x,y).
+
+uncurry g -- это функция f, где f(x,y) -- это значение h y для функции h = g x.
+
+Напишите их реализации.
+*)
+
+let curry f = fun x y -> f(x, y)
+let uncarry f = fun (x, y) -> f(x) y
