b98860619003b3ae5e250796e6a6f75b3bb74cae
Mikhail Zabelin <mike.rzn@yandex.ru>
2021-02-27 15:03:20 +0300

    функция возвращает true/false

--- FunctionalProgramming/FunctionalProgramming/Program24.fs
+++ FunctionalProgramming/FunctionalProgramming/Program24.fs
@@ -5,10 +5,13 @@ type TimeOfDay = { hours: int; minutes: int; f: string }
 //24.4. Задание
 //Время дня может быть представлено тройкой (часы, минуты, половина дня), где часы принимают значение в диапазоне от 0 до 11, минуты -- от 0 до 59, и половина дня -- это либо "AM" (первые 12 часов), либо "PM" (вторые 12 часов).
 //Реализуйте инфиксный оператор .>. , который сравнивает два корректных значения типа TimeOfDay
-let (.>.) x y = 
-    match x.hours.CompareTo(y.hours) with
-    |   0 -> match x.minutes.CompareTo(y.minutes) with
-        |   0 -> x.f.CompareTo(y.f)
-        |   x -> x
-    |   x -> x
-
+let (.>.) x y =
+    match x.f.CompareTo(y.f) with
+    |   1 -> true
+    |   -1 -> false
+    |   0 -> match x.hours.CompareTo(y.hours) with
+        |   1 -> true
+        |   -1 -> false
+        |   0 -> match x.minutes.CompareTo(y.minutes) with
+            |   1 -> true
+            |   _ -> false
