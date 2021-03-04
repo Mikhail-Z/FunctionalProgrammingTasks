fccda272af4bed912477152644d399e8a4b098ef
Mikhail Zabelin <mike.rzn@yandex.ru>
2021-03-04 08:11:37 +0300

    фикс

--- FunctionalProgramming/FunctionalProgramming/Program24.fs
+++ FunctionalProgramming/FunctionalProgramming/Program24.fs
@@ -1,18 +1,15 @@
 ﻿module Program24
 
-type F = 
-  | AM
-  | PM
+type TimeOfDay = { hours: int; minutes: int; f: string }
 
-type TimeOfDay = { hours: int; minutes: int; f: F }
-
-//27. Задание
+//24.4. Задание
 //Время дня может быть представлено тройкой (часы, минуты, половина дня), где часы принимают значение в диапазоне от 0 до 11, минуты -- от 0 до 59, и половина дня -- это либо "AM" (первые 12 часов), либо "PM" (вторые 12 часов).
 //Реализуйте инфиксный оператор .>. , который сравнивает два корректных значения типа TimeOfDay
 let (.>.) x y =
-    match (x.f, y.f) with
-    |   (PM, AM) -> true
-    |   _ -> match x.hours.CompareTo(y.hours) with
+    match x.f.CompareTo(y.f) with
+    |   1 -> true
+    |   -1 -> false
+    |   0 -> match x.hours.CompareTo(y.hours) with
         |   1 -> true
         |   -1 -> false
         |   0 -> match x.minutes.CompareTo(y.minutes) with
