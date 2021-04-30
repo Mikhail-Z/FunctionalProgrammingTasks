705900a5e9a6b92c60bc02670dc844b7d523c6f6
Mikhail Zabelin <mike.rzn@yandex.ru>
2021-04-30 18:02:03 +0300

    сделаное задание 43.3 на map

---
+++ FunctionalProgramming/FunctionalProgramming/Program43.fs
@@ -0,0 +1,10 @@
+﻿module Program43
+
+//43.3. Задание Реализуйте стандартную функцию Map.tryFind самостоятельно в виде функции try_find.
+let try_find key m = 
+    let rec try_get lst = 
+        match lst with
+        |   [] -> None
+        |   (k, value)::tail -> if k = key then Some(value) else try_get tail
+    let r = Map.toList m
+    try_get r
