7cbbdae13513abaa45bdc658cd12b54850f6c1ad
Mikhail Zabelin <mike.rzn@yandex.ru>
2021-05-03 14:59:29 +0300

    сделаное задание 47 на императивное программирование

---
+++ FunctionalProgramming/FunctionalProgramming/Program47.fs
@@ -0,0 +1,26 @@
+﻿module Program47
+
+// 47.4.1
+// Напишите функцию факториала f: int -> int, не используя рекурсию, с помощью императивных возможностей.
+let f n =
+    let mutable acc = 1
+    let accumulate x = acc <- acc * x 
+    let lst = List.ofSeq {1..n}
+    List.iter accumulate lst
+    acc
+
+// 47.4.2
+//Напишите функцию fibo: int -> int, где fibo(n) вычисляет n-е число Фибоначчи (n >= 0), не используя рекурсию, с помощью императивных возможностей.
+//Последовательность Фибоначчи начинается с двух значений 0,1, а n-й элемент равен сумме n-1 - го и n-2 - го элементов: 0,1,1,2,3,5,8,13,...
+let fibo n = 
+    let mutable prevprev = 0
+    let mutable prev = 1
+    let mutable acc = 0
+    let fib() =
+        acc <- prev + prevprev
+        prevprev <- prev
+        prev <- acc
+        
+    for i = 3 to n do
+        fib()
+    acc
